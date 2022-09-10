43092be6805ce18608004731900709ec7a762ff2
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-09-10 16:49:10 +0530

    Create p10.java

---
+++ lecture1/pattern/p10.java
@@ -0,0 +1,43 @@
+public class Main
+{
+  //opposite star pattern. (like this but here i print only star).
+                       1           1
+                       1 2       2 1  
+                       1 2 3   3 2 1
+                       1 2 3 4 3 2 1
+	public static void main(String[] args) {
+		
+		int n=4;
+		int space=2*n-3; // for space.
+	  
+		int i=1;
+		while(i<=n)
+		{
+		    int j=1;
+		    while(j<=i)
+		    {
+		        System.out.print("*");
+		        j++;
+		    }
+		    
+		    int k=1;
+		    while(k<=space)
+		    {
+		        System.out.print(" ");
+		        k++;
+		    }
+		    
+		    int l=1;
+		    while(l<=i)
+		    {
+		        if(l!=n)  // because it print one more digit.(show to avoid it we do it).
+		        System.out.print("*");
+		        
+		        l++;
+		    }
+		    i++;
+	        space-=2;
+		    System.out.println();
+		}
+	}
+}
