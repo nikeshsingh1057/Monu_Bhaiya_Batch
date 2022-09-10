df90e17ad88db97d03a267d8dbaa57da5cf27529
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-09-10 20:13:09 +0530

    Create p13.java

---
+++ lecture1/pattern/p13.java
@@ -0,0 +1,51 @@
+
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+	* * * * * 
+  * *   * * 
+  *       *   //print this star.
+  * *   * * 
+  * * * * *
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int row = 1;
+		int space = -1;
+		int star = n / 2 + 1;
+		while (row <= n) {
+
+			int i = 1;
+			while (i <= star) {
+				System.out.print("* ");
+				i++;
+			}
+			int j = 1;
+			while (j <= space) {
+				System.out.print("  ");
+				j++;
+			}
+			
+			int k = 1;
+			if (row == 1 || row == n) {  // important line if we remove it than it will print one  star extra for row first.
+				k = 2;
+			}
+			while (k <= star) {
+				System.out.print("* ");
+				k++;
+			}
+			if (row <= n / 2) {
+				star--;
+				space += 2;
+			} else {
+				star++;
+				space -= 2;
+			}
+			row++;
+			System.out.println();
+
+		}
+
+	}
+
+}
