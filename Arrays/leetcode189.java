0dffe9d021f54be4264f0487c066a4f6eb2628e4
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-09-21 19:38:05 +0530

    Update leetcode189.java

--- Arrays/leetcode189.java
+++ Arrays/leetcode189.java
@@ -1,2 +1,27 @@
 // rotate array using reversal Algorathim.
 // reversal Algorathim is specially made for this question.
+
+class Solution {
+    public void rotate(int[] arr, int k) {
+        
+        k=k%arr.length;
+        int i=arr.length-k;
+
+        reverse(arr,i,arr.length-1);
+        
+        reverse(arr,0,i-1);
+        
+        reverse(arr,0,arr.length-1);
+    }
+   public static void reverse(int [] arr,int i, int j)
+	{
+		while(i < j)
+		{
+			int temp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+			i++;
+			j--;
+		}
+	}
+}
