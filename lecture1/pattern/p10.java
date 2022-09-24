0ea94375ec149535b74d5c9447efa5622dbf76c5
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-09-25 00:21:05 +0530

    Update p10.java

--- lecture1/pattern/p10.java
+++ lecture1/pattern/p10.java
@@ -1,10 +1,10 @@
 public class Main
 {
   //opposite star pattern. (like this but here i print only star).
-                       1           1
-                       1 2       2 1  
-                       1 2 3   3 2 1
-                       1 2 3 4 3 2 1
+                       *           *
+                       * *       * *  
+                       * * *   * * *
+                       * * * * * * *
 	public static void main(String[] args) {
 		
 		int n=4;
