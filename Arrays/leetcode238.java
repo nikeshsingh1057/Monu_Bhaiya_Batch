97ad6e82b9955ae8944035a860c3728fbfb76416
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-09-24 19:17:57 +0530

    Create leetcode238.java

---
+++ Arrays/leetcode238.java
@@ -0,0 +1,38 @@
+// product of array
+
+import java.util.Arrays;
+
+public class leetcode238 {
+
+	public static void main(String[] args) {
+		
+		int arr[]= {1,2,3,4};
+		
+		System.out.println(Arrays.toString(solve(arr)));
+	}
+	public static int[] solve(int arr[])
+	{
+		int left[]=new int[arr.length];
+		
+		left[0]=1;
+		for(int i=1;i<arr.length;i++)
+		{
+			left[i]=left[i-1]*arr[i-1];
+		}
+		
+		int right[]=new int[arr.length];
+		
+		right[arr.length-1]=1;
+		for(int i=arr.length-2;i>=0;i--)
+		{
+			right[i]=right[i+1]*arr[i+1];
+		}
+		// now finally multiply both left right and right;
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=left[i]*right[i];
+		}
+		return arr;
+	}
+}
