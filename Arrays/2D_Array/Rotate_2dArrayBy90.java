8e436d0422e510a041fac1d6e4446f492fc79362
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-10-20 11:58:51 +0530

    leetcode rotate 2d array by 90 degree.

---
+++ Arrays/2D_Array/Rotate_2dArrayBy90.java
@@ -0,0 +1,34 @@
+// leetcode rotate 2d array by 90 degree.
+
+class Solution {
+    public void rotate(int[][] arr) {
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr[0].length;j++)  // we can aslo put j=i but dianonal elmenent not chance go no need to j=1 so we do j=j+1.
+            {
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+        
+        // reverse of matrix according to row wise.
+        for(int i=0;i<arr.length;i++)
+            Reverse(arr[i]);  // 1st row ka address bhej rahe hai
+
+    }
+    public static void Reverse(int [] arr)
+    {
+        int i=0;
+        int j=arr.length-1;
+        while(i<j)
+        {
+            int temp =arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
