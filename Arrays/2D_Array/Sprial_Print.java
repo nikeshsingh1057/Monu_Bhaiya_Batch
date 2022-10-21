b93e7014b1c9b089c86c87c1e8dba27e1bb5b629
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-10-22 00:28:17 +0530

    Sprial printing of matrix

--- Arrays/2D_Array/Sprial_Print.java
+++ Arrays/2D_Array/Sprial_Print.java
@@ -1,12 +1,15 @@
 // Sprial print of 2d Array
 
-package Arrays;
+// 1st type sprial matrix.
 
 public class Sprial_Print {
 
 	public static void main(String[] args) {
 	
-		int [][] arr= {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+		int [][] arr= {{1,2,3,4},    
+			       {5,6,7,8},       // O/P -> 1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10
+			       {9,10,11,12},
+			       {13,14,15,16}};
 		PrintSprial(arr);
 		
 	}
@@ -47,3 +50,55 @@ public class Sprial_Print {
 		}
 	}
 }
+
+// sprial matrix type II .
+
+public class Sprial_Print_II {
+
+	public static void main(String[] args) {
+		
+		int[][] arr = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+		PrintSprial(arr);
+	}
+	public static void PrintSprial(int [][] arr)
+	{
+		int min_row=0;
+		int min_col=0;
+		int max_row=arr.length-1;
+		int max_col=arr[0].length-1;
+		int total=arr.length*arr[0].length;    // row*column
+		int count=0;
+		while(count<total)
+		{
+			               // left wall
+			for(int i=min_row, j=min_col;i<=max_row && count<total;i++)
+			{
+				System.out.print(arr[i][j]+" ");
+				count++;
+			}
+			min_col++;
+			              // bottom wall
+			for(int i=min_col,j=max_row;i<=max_col && count<total;i++)
+			{
+				System.out.print(arr[j][i]+" ");
+				count++;
+			}
+			max_row--;
+			              // right wall
+			for(int i=max_row,j=max_col;i>=min_row && count<total;i--)
+			{
+				System.out.print(arr[i][j]+" ");
+				count++;
+			}
+			max_col--;			  // top wall
+			for(int i=max_col,j=min_row;i>=min_col && count<total;i--)
+			{
+				System.out.print(arr[j][i]+" ");
+				count++;
+			}
+			min_row++;
+		}
+	}
+
+}
+
