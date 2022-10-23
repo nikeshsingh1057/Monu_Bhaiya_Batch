f96ca92ecaee0ecf3909664fd605ebf4b5724bdd
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-10-23 10:54:59 +0530

    Update mySqrt.java

--- Arrays/Binary Search/mySqrt.java
+++ Arrays/Binary Search/mySqrt.java
@@ -1,6 +1,6 @@
 // find the squre root using binary Search
 // same as kth root just remove k=2
-
+// Type 1--> if return in form of int.
 class Solution {
     public int mySqrt(int x) {
       
@@ -25,5 +25,50 @@ class Solution {
                 high=n-1;
         }
         return ans;
+    }
+}
+
+// type 2 --> if return in double with given precision.
+
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        
+        Scanner S = new Scanner(System.in);
+        int t =S.nextInt();
+
+        while(t-->0){
+
+            long n = S.nextLong();
+            long lower = 1;
+            long higher = n;
+            double root=0.0;
+            while(lower<=higher){
+
+                long mid =lower + (higher-lower)/2;
+                if(mid*mid <= n)
+                {
+                    root=mid;
+                    lower=mid+1;
+                }
+                else
+                {
+                    higher = mid - 1;
+                }
+
+            }
+            double incr=0.1;
+            for(int i=0;i<4;i++)
+            {
+                while(root*root<=n)
+                {
+                    root+=incr;
+                }
+                root-=incr;
+                incr/=10;
+            }
+            System.out.printf("%.4f\n",root);
+        }
+
     }
 }
