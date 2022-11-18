60889ea11157d2cd66a9d12e5e943004e9beaca9
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-11-18 18:26:00 +0530

    comare string builder and normal stirng.

---
+++ String/String_Vs_StringBulider.java
@@ -0,0 +1,28 @@
+// comare string builder and normal stirng.
+
+public class String_Vs_StringBulider {
+
+	public static void main(String[] args) {
+		
+		StringCom();
+//		StringBuilderCom();
+
+	}
+
+	public static void StringCom() {
+		String s = "";
+		for (int i = 0; i < 100000; i++) {
+			s = s + i;
+		}
+		System.out.println(s);
+	}
+
+	public static void StringBuilderCom() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 100000; i++) {
+			sb.append(i);
+		}
+		System.out.println(sb);
+	}
+
+}
