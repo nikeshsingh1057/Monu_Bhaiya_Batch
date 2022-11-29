bb39f938da5bd5db9284f1f5c09a480df977f227
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2022-11-30 00:47:26 +0530

    minimum flip of 'a' ans 'b' so that String make equal.

--- Sliding_Window/Flip_a_and_b.java
+++ Sliding_Window/Flip_a_and_b.java
@@ -11,9 +11,9 @@ public class Main {
         String str=sc.next();
 
         int flip_a=Flip_Char(str,k,'b','a');
-		int flip_b=Flip_Char(str,k,'a','b');
+	int flip_b=Flip_Char(str,k,'a','b');
 		
-		System.out.println(Math.max(flip_a,flip_b));
+	System.out.println(Math.max(flip_a,flip_b));
 
     }
 
@@ -38,6 +38,46 @@ public class Main {
 			max=i-j+1;
 			ans=Math.max(max, ans);
 			i++;
+		}
+		return ans;
+	}
+}
+
+
+
+// method 2 only passing single charector.
+
+public class Kartik_Bhaiya_and_String {
+
+	public static void main(String[] args) {
+		
+		String str="abba";
+		int k=2;
+		
+	    int a_filp=Perfetness_Maximum_length(str,k,'a');
+		int b_filp=Perfetness_Maximum_length(str,k,'b');
+		
+		System.out.println(Math.max(a_filp, b_filp));
+	}
+	public static int Perfetness_Maximum_length(String str,int k,char ch)
+	{
+		int flip=0;
+		int si=0;
+		int ei=0;
+		int ans=0;
+		while(ei<str.length())
+		{
+			if(str.charAt(ei)==ch)
+				flip++;
+			
+			while(flip>k)
+			{
+				if(str.charAt(si)==ch)
+					flip--;
+				si++;
+			}
+			ans=Math.max(ans, ei-si+1);
+			ei++;
 		}
 		return ans;
 	}
