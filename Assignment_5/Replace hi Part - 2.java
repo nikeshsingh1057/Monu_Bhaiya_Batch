bcc09a9ebb2a6b2d710b29ac6a663d9fba6590fd
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-01-21 23:51:28 +0530

    Count , Remove , Replace hi Part - 2
    Take as input str, a string.
    
    a.) Write a recursive function which counts the number of times ‘hi’ appears in the string – but skip all such ‘hi’ which are followed by ‘t’ to form ‘hit’. Print the value returned.
    
    b.) Write a recursive function which removes all ‘hi’ in the string – but skip all such ‘hi’ which are followed by ‘t’ to form ‘hit’. Print the value returned.
    
    c.) Write a recursive function which replaces all ‘hi’ in the string with ‘bye’ – but skip all such ‘hi’ which are followed by ‘t’ to form ‘hit’. Print the value returned.
    
    Input Format
    Enter the String
    
    Constraints
    None
    
    Output Format
    Display the total no.of ''hi'' (''hi'' should not be followed by a 't'), string in which all ''hi'' are removes(''hi'' should not be followed by a 't'), string in which all ''hi'' are replace by ''bye''(''hi'' should not be followed by a 't')
    
    Sample Input
    abchihitfhi
    Sample Output
    2
    abchitf
    abcbyehitfbye

---
+++ Assignment_5/Replace hi Part - 2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main {
+
+    static int count;
+    public static void main(String args[]) {
+        
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        count=0;
+        solve(str,0,"","");
+    }
+   
+    public static void solve(String str,int i,String ans1,String ans2)
+    {
+        if(i==str.length())
+        {
+            System.out.println(count);
+            System.out.println(ans1);
+            System.out.println(ans2);
+            return ;
+        }
+        if(i<str.length()-2 && str.charAt(i)=='h' && str.charAt(i+1)=='i' && str.charAt(i+2)!='t'|| i==str.length()-2 && str.charAt(i)=='h' && str.charAt(i+1)=='i'){
+
+            count++;
+            ans2+="bye";
+            solve(str,i+2,ans1,ans2);
+        }
+        else{
+
+            ans1+=str.charAt(i);
+            ans2+=str.charAt(i);
+            solve(str,i+1,ans1,ans2);
+        }
+    }
+}
