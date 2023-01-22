86f31e28b21369463c474c6f7fb53d201f4286c4
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-01-22 21:43:42 +0530

    Subset_problem ( Recursion )
    Take an input N, a number. Take N more inputs and store that in an array. Take an input target, a number
    
    a. Write a recursive function which prints subsets of the array which sum to target.
    
    b. Write a recursive function which counts the number of subsets of the array which sum to target. Print the value returned.
    
    Input Format
    Take an input N, a number. Take N more inputs and store that in an array. Take an input target, a number
    
    Constraints
    None
    
    Output Format
    Display the number of subsets and print the subsets in a space separated manner.
    
    Sample Input
    3
    1
    2
    3
    3
    Sample Output
    1 2  3
    2
    Explanation
    Add 2 spaces between 2 subset solutions

---
+++ Assignment_5/Subset_problem.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Main {
+    public static int count;
+    public static void main(String args[]) {
+        
+        count=0;
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+
+        for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+
+        int k=sc.nextInt();
+        SubsetPrint(arr,0,"",0,k);
+        System.out.print("\n"+count);
+    }
+    public static void SubsetPrint(int arr[],int i,String ans,int sum,int k)
+    {
+        if(sum==k){
+            count++;
+            System.out.print(ans+" "); 
+            return;
+        }
+
+        if(i>=arr.length)
+        {
+            return;
+        }
+        int a=arr[i];
+
+        SubsetPrint(arr, i+1, ans+a+" ",sum+a,k);
+        SubsetPrint(arr, i+1, ans,sum,k);
+       
+        
+    }
+}
