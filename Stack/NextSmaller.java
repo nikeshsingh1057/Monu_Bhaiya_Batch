d7da805cce2ff79a59c8629cf375c3e3d4dc23c4
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-04-09 17:08:32 +0530

    find next smaller of array element

---
+++ Stack/NextSmaller.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+public class Main
+{
+    
+	public static void main(String[] args) {
+		
+		int arr[]={0,3,5,4,6,1,7};              // next Samller of arr is ->> [-1 1 4 1 1 -1 -1]
+		
+		int ans[]=new int[arr.length];
+		
+		Stack<Integer> st = new Stack<>();
+		
+		for(int i=arr.length-1;i>=0;i--)
+		{
+		    while(!st.isEmpty() && st.peek()>arr[i])
+		    {
+		        st.pop();
+		    }
+		    if(st.isEmpty())
+		        ans[i]=-1;
+		        
+		    else 
+		        ans[i]=st.peek();
+		    
+		    st.push(arr[i]);
+		}
+		
+		// for displaying answer.
+		for(int i=0;i<arr.length;i++)
+		{
+		  System.out.print(ans[i]+" ");
+		}
+	}
+}
