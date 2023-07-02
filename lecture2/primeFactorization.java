538cc369ec4f7f56e4ca5c6e63a70413c2f062aa
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-07-02 10:19:15 +0530

    prime factorization

--- lecture2/primeFactorization.java
+++ lecture2/primeFactorization.java
@@ -1,12 +1,17 @@
 //finding prime factorization
 
-        while(n!=1) 
-        {
-           if(n%i==0)
-           {
-             System.out.print(i+" ");
-               n=n/i;
-           }
-           else 
-           i++;
-        }
+public static void main(String[] args) {
+		
+        int n=100;
+	int i=1;
+	 while(n!=1) 
+	{
+	        if(n%i==0)
+	        {
+	                System.out.print(i+" ");
+	               n=n/i;
+	        }
+	         else 
+	           i++;
+	}
+}
