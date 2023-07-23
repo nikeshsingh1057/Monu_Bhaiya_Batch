0c5b7adf11acaa380f1c5dc6a4bcadb875f12858
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-07-23 22:19:46 +0530

    Dynamic_Queue

---
+++ Stack/Dynamic_Queue .java
@@ -0,0 +1,22 @@
+package Stack;
+
+public class Dynamic_Queue extends Queue{
+		
+	@Override
+	public void Enqueue(int item) throws Exception{
+		
+		if(this.isFull()){
+			int [] a=new int[2*arr.length];
+			
+			for(int i=0;i<arr.length;i++)
+			{
+				int idx=(front+i)%arr.length;
+				a[i]=arr[idx];
+			}
+			
+			front=0;
+			arr=a;
+		}
+		super.Enqueue(item);
+	}
+}
