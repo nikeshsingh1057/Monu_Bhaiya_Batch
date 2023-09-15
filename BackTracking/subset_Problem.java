516916b39f7c94ab76ab58a24ad1fe88aadb79b0
nikeshsingh1057 <94050485+nikeshsingh1057@users.noreply.github.com>
2023-09-15 14:34:49 +0530

    subset_Problem_subsequance.

--- BackTracking/subset_Problem.java
+++ BackTracking/subset_Problem.java
@@ -1,43 +1,58 @@
-package BackTracking;
+// subset l leetcode_78
+
+class Solution {  // see on youtube isi variation par bahut question hai. with simple modification
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ll= new ArrayList<>();
+        subsetPrint(ans,ll,nums,0);
+        return ans;
+    }
+    public static void subsetPrint(List<List<Integer>> ans,List<Integer> ll,int arr[],int idx){
+        
+        ans.add(new ArrayList<>(ll));
+        if(idx==arr.length){
+            return;
+        }
+        
+        for(int i=idx;i<arr.length;i++){
+            
+            ll.add(arr[i]);
+            subsetPrint(ans,ll,arr,i+1);
+            ll.remove(ll.size()-1);
+        }
+    }
+}
+
+// subset 2 leetcode_90
 
 import java.util.*;
 
 public class subset_2 {
-	
-	
-	
 	public static void main(String[] args) {
-		
-		
-		 List<List<Integer>> ans = new ArrayList<>();
+
+	     List<List<Integer>> ans = new ArrayList<>();
 	     List<Integer> ll= new ArrayList<>();
 	      
 	     int nums[]= {1,2,2.4};
 	      Arrays.sort(nums);
 	        
 	     Subset_2(nums,0,ans,ll);
-	     
-		System.out.println(ans);
-		
+		System.out.println(ans);	
 	}
 	  public static void Subset_2(int [] arr,int idx, List<List<Integer>> ans,List<Integer> ll)
 	    {
-	        ans.add(new ArrayList<>(ll));
-	        
+	        ans.add(new ArrayList<>(ll));  
 	        if(idx==arr.length)
 	        {
 	            return;
 	        }
-	        
 	        for(int i=idx;i<arr.length;i++)
 	        {
 	            if(i>idx && arr[i]==arr[i-1])  // subset ke case me pahala dublicate allow hai but dusara do discard karna hai.     i>idz because first ko rakhana his second ko discard.
 	                continue;
-	            
 	            ll.add(arr[i]);
-	            
 	            Subset_2(arr,i+1,ans,ll);
-	            
 	            ll.remove(ll.size()-1);
 	        }
 	    }
