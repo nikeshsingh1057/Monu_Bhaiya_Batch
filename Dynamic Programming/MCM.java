b3a82ef458a90ff9beca72bd772e95f3fe54fa6e
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2023-11-03 18:25:06 +0530

    Matrix_Chain_Multiplication

---
+++ Dynamic Programming/MCM.java
@@ -0,0 +1,27 @@
+package DynamicProgramming;
+
+public class Matrix_Chain_Multiplication {
+
+	public static void main(String[] args) {
+		
+		int [] arr= {4,2,3,5,1};
+		System.out.println(mcm(arr,0,arr.length-1));
+
+	}
+	// only recursion
+	public static int mcm(int [] arr,int i,int j) {
+		
+		if(i+1==j)
+			return 0;
+		
+		int ans=Integer.MAX_VALUE;
+		for(int k=i+1;k<j;k++) {
+			
+			int fs=mcm(arr,i,k);
+			int ss=mcm(arr,k,j);
+			int self=arr[i]*arr[j]*arr[k];
+			ans=Math.min(ans, fs+ss+self);
+		}
+		return ans;
+	}
+}
