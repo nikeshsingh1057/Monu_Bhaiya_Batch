a80a75db778407f4fd5d8937d1795b350368c751
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2023-11-06 18:42:06 +0530

    rat_cheese.java
    //https://hack.codingblocks.com/app/contests/5184/525/problem
    
    You are given an N*M grid. Each cell (i,j) in the grid is either blocked, or empty. The rat can move from a position towards left, right, up or down on the grid.
    Initially rat is on the position (1,1). It wants to reach position (N,M) where it's cheese is waiting for. If a path exists-it is always unique. Find that path and help the rat reach its cheese.
    
    Input Format
    First line contains 2 integers N and M denoting the rows and columns in the grid.
    Next N line contains M characters each. An 'X' in position (i,j) denotes that the cell is blocked and ans 'O' denotes that the cell is empty.
    
    Constraints
    1 <= N , M <= 10
    
    Output Format
    Print N lines, containing M integers each. A 1 at a position (i,j) denotes that the (i,j)th cell is covered in the path and a 0 denotes that the cell is not covered in the path.
    If a path does not exits then print "NO PATH FOUND"
    
    Sample Input
    5 4
    OXOO
    OOOX
    XOXO
    XOOX
    XXOO
    Sample Output
    1 0 0 0 
    1 1 0 0 
    0 1 0 0 
    0 1 1 0 
    0 0 1 1

--- Assignment_5/rat_cheese.java
+++ Assignment_5/rat_cheese.java
@@ -91,3 +91,70 @@ op
 
 */
 
+
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+// https://hack.codingblocks.com/app/contests/5184/525/problem
+
+import java.util.*;
+public class Main {
+    static boolean f=false;
+    public static void main(String args[]) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+
+        char maze[][]=new char[n][m];
+        for(int i=0;i<n;i++)
+        {
+            String s=sc.next();
+            for(int j=0;j<m;j++)
+            {
+                maze[i][j]=s.charAt(j);
+            }
+        }
+        int ans[][]=new int[n][m];
+
+        PrintPath(maze,0,0,ans);
+
+        if(f==false)
+            System.out.println("NO PATH FOUND");
+       
+    }
+    public static void PrintPath(char[][] maze,int cr,int cc,int ans[][])
+    {
+        if(cr==maze.length-1 && cc==maze[0].length-1 &&  maze[cr][cc]!='X')
+        {
+            f=true;
+            ans[cr][cc]=1;
+            display(ans);
+            ans[cr][cc]=0;
+            return;
+        }
+        if(cr<0 || cc<0 || cr>=maze.length || cc>=maze[0].length || maze[cr][cc]=='X'){
+
+            return;
+        }
+        maze[cr][cc]='X';
+        ans[cr][cc]=1;
+
+        PrintPath(maze,cr-1,cc,ans);//up
+        PrintPath(maze,cr+1,cc,ans);//down
+        PrintPath(maze,cr,cc+1,ans);//right
+        PrintPath(maze,cr,cc-1,ans);//left
+
+        maze[cr][cc]='O';
+        ans[cr][cc]=0;
+    }
+    public static void display(int arr[][])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[0].length;j++)
+            {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
