e16d2083df14209fbaf1e5ffd822f76ce75fa36a
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-01-02 23:09:15 +0530

    Kadanes_Algo.java

--- Arrays/Kadanes_Algo.java
+++ Arrays/Kadanes_Algo.java
@@ -30,4 +30,57 @@ public class Kadanes_Algo {
 }
 
 /* question based on kadhan's algo */
+
 //1.   https://codeforces.com/contest/1899/problem/C
+//2.   https://www.geeksforgeeks.org/problems/largest-sum-subarray-of-size-at-least-k3121/1
+
+//2nd  ka answer
+
+class GFG {
+
+// function to find the largest subarray sum
+// with atleast k elements in it
+// using Kadane's algorithm
+static long maxSumWithK(long[] a, long n, int k)
+{
+
+	// calculating sum of the first k elements
+	// of the array
+	long sum = 0;
+	for (int i = 0; i < k; i++) {
+	sum += a[i];
+	}
+
+	long last = 0;
+	int j = 0;
+	long ans = Long.MIN_VALUE;
+	ans = Math.max(ans, sum);
+
+	// iterating over the subarrays
+	// and updating the sum accordingly
+	for (int i = k; i < n; i += 1) {
+	sum = sum + a[i];
+	last = last + a[j++];
+
+	// using sliding window
+	// to update the ans
+	ans = Math.max(ans, sum);
+	if (last < 0) {
+		sum = sum - last;
+		ans = Math.max(ans, sum);
+		last = 0;
+	}
+	}
+	return ans;
+}
+public static void main(String[] args) {
+	long[] arr = { 1, 2, 3, -10, -3 };
+	int k = 4;
+	long n = arr.length;
+
+	// Function Call
+	System.out.println(maxSumWithK(arr, n, k));
+}
+}
+
+
