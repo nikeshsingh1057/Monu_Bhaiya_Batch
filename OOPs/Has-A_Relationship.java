41963b9abbe08b133b24df6779dfda437a749f0a
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-01-27 18:51:59 +0530

    Has-A_Relationship

---
+++ OOPs/Has-A_Relationship.java
@@ -0,0 +1,21 @@
+                     // HAS- A relation ship
+
+class engine{
+    
+    void car(){
+        System.out.print("Has -A relation ship hai ");
+    }
+    
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		engine e=new engine();
+		e.car();
+	}
+}
+
+
+// note inheritacne ke case me is-A reationship hota hai 
