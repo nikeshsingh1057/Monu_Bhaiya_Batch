67bdd19e80882bb2e3df8aefc0c510e04e6ddd2e
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-01-27 12:43:46 +0530

    static Keyword_1

--- OOPs/staticKeyword_1.java
+++ OOPs/staticKeyword_1.java
@@ -2,6 +2,8 @@
 
 // note: static variable belongs to class not to object similarly static method belongs to class not to object.
 // we cannot create outer static class but we can create innner static class.
+// note : agar same class ke ander static method hai then class name bhee likne ke need nahi dircet method ke name se method call ho jata hai.
+
 
 class test{
     
@@ -30,5 +32,15 @@ public class Main
 		test.car1();
 		test.car2();
 		System.out.println(test.a);
+	}
+	public static void bus(){
+		// isko call karne ke liye Main ka object create nahi karna hai only bus() name se call ho jayega since bus() method static ke
+		// ander hai i.e within the call hai so dircet call ho jayega.
+		System.out.println("Main class ke ander bus() method and static bhee  hai aur within class hai so dircect call ho jayega");
+	}
+	public void bus2(){
+		System.out.print("ye method within class ke ander hai lekin isko call karne  ke lime main ka object create karna hoga.");
+		// i.e -> Main m=new Main();
+	 	//        m.bus2();
 	}
 }
