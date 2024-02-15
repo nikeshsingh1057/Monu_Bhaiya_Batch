be54ab60fca14f3a0868350e707b5d873310e5aa
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-02-15 23:01:32 +0530

    Putting One Class Refernence To Other Class

---
+++ OOPs/PuttingOneClassRefernenceToOtherClass.java
@@ -0,0 +1,135 @@
+// eak class ka reference dusere class me rakhana .
+// maximum width of binary tree question me use hua hai ye mehtod leetcode_662
+class TreeNode {
+      
+      int val;
+      TreeNode(int val){
+          this.val=val;
+      }
+}
+
+class Pair{
+    
+    TreeNode node;
+    int idx;
+    
+    Pair(TreeNode node,int idx){
+        this.idx=idx;
+        this.node=node;
+    }
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+	    
+	    TreeNode t1=new TreeNode(5);
+	    TreeNode t2=new TreeNode(15);
+	    TreeNode t3=new TreeNode(50);
+	    TreeNode t4=new TreeNode(55);
+	    
+	    Pair p1=new Pair(t1,10);
+	    Pair p2=new Pair(t2,30);
+	    Pair p3=new Pair(t3,100);
+	    Pair p4=new Pair(t4,110);
+	    
+		System.out.println(t1+" "+t1.val+" "+p1.node+" "+p1.node.val);
+		System.out.println(t2+" "+t2.val+" "+p2.node+" "+p2.node.val);
+		System.out.println(t3+" "+t3.val+" "+p3.node+" "+p3.node.val);
+		System.out.println(t4+" "+t4.val+" "+p4.node+" "+p4.node.val);
+	}
+}
+
+
+class TreeNode {
+      
+      int val;
+      TreeNode(int val){
+          this.val=val;
+      }
+}
+
+class Pair{
+    
+    TreeNode node;
+    int idx;
+    
+    Pair(TreeNode node,int idx){
+        this.idx=idx;
+        this.node=node;
+    }
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+	    
+	    TreeNode t1=new TreeNode(5);
+	    TreeNode t2=new TreeNode(15);
+	    TreeNode t3=new TreeNode(50);
+	    TreeNode t4=new TreeNode(55);
+	    
+	    Pair p1=new Pair(t1,10);
+	    Pair p2=new Pair(t2,30);
+	    Pair p3=new Pair(t3,100);
+	    Pair p4=new Pair(t4,110);
+	    
+		System.out.println(t1+" "+t1.val+" "+p1.node+" "+p1.node.val);
+		System.out.println(t2+" "+t2.val+" "+p2.node+" "+p2.node.val);
+		System.out.println(t3+" "+t3.val+" "+p3.node+" "+p3.node.val);
+		System.out.println(t4+" "+t4.val+" "+p4.node+" "+p4.node.val);
+	}
+}
+
+class TreeNode {
+      
+      int val;
+      TreeNode(int val){
+          this.val=val;
+      }
+}
+
+class Pair{
+    
+    TreeNode node;
+    int idx;
+    
+    Pair(TreeNode node,int idx){
+        this.idx=idx;
+        this.node=node;
+    }
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+	    
+	    TreeNode t1=new TreeNode(5);
+	    TreeNode t2=new TreeNode(15);
+	    TreeNode t3=new TreeNode(50);
+	    TreeNode t4=new TreeNode(55);
+	    
+	    Pair p1=new Pair(t1,10);
+	    Pair p2=new Pair(t2,30);
+	    Pair p3=new Pair(t3,100);
+	    Pair p4=new Pair(t4,110);
+	    
+		System.out.println(t1+" "+t1.val+" "+p1.node+" "+p1.node.val);
+		System.out.println(t2+" "+t2.val+" "+p2.node+" "+p2.node.val);
+		System.out.println(t3+" "+t3.val+" "+p3.node+" "+p3.node.val);
+		System.out.println(t4+" "+t4.val+" "+p4.node+" "+p4.node.val);
+	}
+}
+
+/*
+TreeNode@2d6a9952 5 TreeNode@2d6a9952 5
+TreeNode@22a71081 15 TreeNode@22a71081 15
+TreeNode@3930015a 50 TreeNode@3930015a 50
+TreeNode@629f0666 55 TreeNode@629f0666 55
+*/
