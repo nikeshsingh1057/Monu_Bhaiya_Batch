4c0f207eaf43fc7a3e0cbc301d686587686ff792
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-04-24 09:21:11 +0530

    LeetCode_1063 (Number of Valid Subarrays)
    https://leetcode.ca/all/1063.html

--- Stack/LeetCode_1063.java
+++ Stack/LeetCode_1063.java
@@ -9,7 +9,7 @@ The leftmost element of the subarray is not larger than other elements in the su
 
 public class LeetCode_1063 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {  // monu bahiya logic
 		
 		int arr[]= {1,4,2,5,3};
 		int ans=0;
@@ -27,3 +27,70 @@ public class LeetCode_1063 {
 	}
 
 }
+
+// method 2 (pepcoding) --------------------------------------------------------------------------------------------------------------
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,4,2,5,3};
+		int nextSM[] = nextSmallerElement(arr,arr.length);
+		
+		int ans = 0;
+		for(int i=0;i<arr.length;i++)
+		    ans += nextSM[i]-i;          // now index se substract kar denge wahi answer hoga.
+		
+		System.out.println(ans);
+	}
+    // finding just next smaller of current element in array
+    public static int[] nextSmallerElement(int arr[], int n) 
+    { 
+	int nextSM[]=new int[n];    //nextSM->next smaller
+        Stack<Integer> st=new Stack<>();
+        
+        for(int i=n-1;i>=0;i--){
+            
+            while(!st.isEmpty() && arr[i]<=arr[st.peek()])
+                st.pop();
+            
+            if(!st.isEmpty())
+                nextSM[i]=st.peek();   // index put karenge.
+            else
+                nextSM[i]=n; // agar koi smaller nahi mila then n put kar denge.
+            st.push(i);
+        }
+        return nextSM;
+	} 
+}
+
+// method 3rd (optimize 2nd method ) pepcoding. --------------------------------------------------------------------------------------
+
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,4,2,5,3};
+		int n=arr.length;
+		int ans=0;
+		
+        Stack<Integer> st=new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i])
+                st.pop();
+            
+            int si=n;          // agar next smaller nahi milta hai then array ka size lenge.
+            if(!st.isEmpty())
+                si=st.peek();   // next smaller index si me put kar lenge
+                
+            ans += (si-i);
+            st.push(i);
+        }
+        System.out.println(ans);
+	}
+}
+
