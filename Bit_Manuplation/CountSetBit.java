7dd768b47b099e63882b6d11b978ff9a8a9460db
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-05-31 12:46:02 +0530

    Count Set Bit or check it bit is 0 or 1

--- Bit_Manuplation/CountSetBit.java
+++ Bit_Manuplation/CountSetBit.java
@@ -56,6 +56,11 @@ public class CountSetBit {
 			if((n&(1<<i)) != 0)
 				count++;
 		}*/
+
+		/*for(int i=0;i<32;i++) { 
+			if((n>>i) & 1 != 0)
+				count++;
+		}*/
 		System.out.println(count);
 	}
 }
