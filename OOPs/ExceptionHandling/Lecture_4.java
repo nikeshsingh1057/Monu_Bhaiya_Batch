2e07ca34eca1303545eb1326e17a3820fd0b72e9
Nikesh Singh <94050485+nikeshsingh1057@users.noreply.github.com>
2024-06-30 22:26:19 +0530

    Types of ways to print Exception in catch block

---
+++ OOPs/ExceptionHandling/Lecture_4.java
@@ -0,0 +1,21 @@
+                                    /*Tyeps of ways to print Exception in catch */
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		try{
+		    
+		    int a=100;
+		    int b=0;
+		    int c=a/b;
+		    System.out.println(c);
+		}
+		catch(Exception e){
+		    
+		    e.printStackTrace(); // 1st way -> ye best tarika hai Exception print karne ka.
+		    System.out.println(e); // 2nd way
+		    System.out.println(e.toString()); // 3rd way   (2nd and 3rd same output dega.)
+		}
+	}
+}
